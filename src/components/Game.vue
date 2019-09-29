e5e745c7427d0e9fc511d32232acb4574714b600
Fabio Z Tessitore <fabioztessitore@gmail.com>
2019-09-29 14:24:03 +0200

    test human class

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,35 @@
 <template>
     <div class="game">
         Game is running: {{ isRunning }}
-        
+
+        <div class="test">
+            <button @click="decrease">Decrease life</button>
+            <p>Human Life: {{ human.life }}</p>
+        </div>
     </div>
 </template>
 
 <script>
+import Human from '../models/Human'
+
 export default {
     data: function () {
         return {
-            isRunning: false
+            isRunning: false,
+            human: new Human(100)
+        }
+    },
+
+    methods: {
+        decrease () {
+            this.human.decreaseLife()
         }
     }
 }
 </script>
+
+<style scoped>
+.test {
+    font-size: 2rem;
+}
+</style>
---
+++ src/models/Human.js
@@ -0,0 +1,11 @@
+class Human {
+    constructor (life) {
+        this.life = life
+    }
+
+    decreaseLife () {
+        this.life--
+    }
+}
+
+export default Human
